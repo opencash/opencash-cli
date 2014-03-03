a833254fb62f42eebd2237d227e280f877c7b403
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-03-02 22:54:07 -0300

    Travis failure diagnosis

--- travis-diagnostics.sh
+++ travis-diagnostics.sh
@@ -2,16 +2,10 @@
 
 echo "PATH: $PATH"
 
-echo "GCC gcc version: `gcc --version`"
-echo "GCC gcc dumpmachine: `gcc -dumpmachine`"
+echo "g++ at `which g++`"
 echo "GCC g++ version: `g++ --version`"
 echo "GCC g++ dumpmachine: `g++ -dumpmachine`"
 
-echo "Clang clang version: `clang --version`"
-echo "Clang clang dumpmachine: `clang -dumpmachine`"
+echo "clang++ at `which clang++`"
 echo "Clang clang++ version: `clang++ --version`"
 echo "Clang clang++ dumpmachine: `clang++ -dumpmachine`"
-
-sudo apt-get update
-echo "apt-get search gcc: `apt-cache search gcc`"
-echo "apt-get search clang: `apt-cache search gcc`"
