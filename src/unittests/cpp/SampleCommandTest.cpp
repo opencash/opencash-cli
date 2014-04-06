f09a290a8f269fd04f595414adb33c9745408074
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-04-06 15:49:53 -0300

    Compiles with latest libopencash

--- src/unittests/cpp/SampleCommandTest.cpp
+++ src/unittests/cpp/SampleCommandTest.cpp
@@ -4,13 +4,21 @@ using namespace testing;
 
 #include "SampleCommand.h"
 #include <opencash/api/Api.h>
+#include <opencash/api/ApiExceptions.h>
 
 using SampleCommand = opencash::cli::SampleCommand;
 using AppContext = opencash::cli::AppContext;
+using FileAlreadyExists = opencash::api::FileAlreadyExists;
 
 class MockApi : public opencash::api::Api {
   public:
-    MOCK_CONST_METHOD1(createSampleFile, void(const std::string& outputFileName));
+    MOCK_CONST_METHOD1(mockCreateSampleFile, void(const std::string&));
+
+    virtual void createSampleFile(const std::string& fileName)
+      const throw(FileAlreadyExists) override
+    {
+      mockCreateSampleFile(fileName);
+    }
 };
 
 TEST(SampleCommand, shouldDelegateToApi) {
@@ -20,7 +28,7 @@ TEST(SampleCommand, shouldDelegateToApi) {
   const char *argv[] = {"sample", "-o", "sample.opencash"};
 
   // expect
-  EXPECT_CALL(mockApi, createSampleFile("sample.opencash"));
+  EXPECT_CALL(mockApi, mockCreateSampleFile("sample.opencash"));
 
   // when
   SampleCommand(ctx, mockApi).run(3, argv);
